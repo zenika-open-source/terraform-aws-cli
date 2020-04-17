cbb7572149e94231c58025246d36400f696cce3f
bgauduch <bgauduchon@talend.com>
2020-04-17 17:47:36 +0200

    :white_check_mark: added Dockerfile linting script

---
+++ scripts/lint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eo pipefail 
+
+if [ -f /.dockerenv ]; then
+  hadolint Dockerfile
+else
+  docker run -a stdout -a stderr -v $(pwd):/host:ro hadolint/hadolint:v1.17.2-debian hadolint /host/Dockerfile
+fi
