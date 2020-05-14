1f89e5a0f77e0a8ba53b4f1f40008ab1738345e6
Baptiste Gauduchon <bgauduch@users.noreply.github.com>
2020-05-14 10:40:16 +0200

    :arrow_up: Update Terraform CLI version (#27)
    * :construction_worker: add new tf cli version and correct image output name
    
    * :pencil: remove microbadger brocken badge and update tagging strategy
    
    * :whale: bumped git version and default CLI version
    
    * :wrench: reverted dev script ability to build without args

--- dev-build.sh
+++ dev-build.sh
@@ -6,6 +6,6 @@ if [ -n "$1" ] && [ -n "$2" ] ; then
   echo "Building images with parameters AWS_CLI_VERSION=${1} and TERRAFORM_VERSION=${2}"
   docker image build --build-arg AWS_CLI_VERSION="$1" --build-arg TERRAFORM_VERSION="$2" -t zenika/terraform-aws-cli:latest .
 else
-  echo "Please provide AWS_CLI_VERSION and TERRAFORM_VERSION parameters"
-  echo "Usage: ./dev-build.sh ${AWS_CLI_VERSION} ${TERRAFORM_VERSION}"
+  echo "Building images with default parameters"
+  docker image build -f Dockerfile -t zenika/terraform-azure-cli:dev .
 fi
